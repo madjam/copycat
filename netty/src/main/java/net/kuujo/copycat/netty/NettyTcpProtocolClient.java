14cc065ee5a0ac2e5f9d11bd45c2d268a7b55d69
Madan Jampani <madan@onlab.us>
2015-04-24 18:46:22 -0700

    Made NettyTcpProtocolClient thread-safe

--- netty/src/main/java/net/kuujo/copycat/netty/NettyTcpProtocolClient.java
+++ netty/src/main/java/net/kuujo/copycat/netty/NettyTcpProtocolClient.java
@@ -17,7 +17,6 @@ package net.kuujo.copycat.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
-import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -32,10 +31,14 @@ import net.kuujo.copycat.protocol.ProtocolException;
 
 import javax.net.ssl.SSLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.ByteBuffer;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 
 /**
@@ -44,21 +47,16 @@ import java.util.function.Supplier;
  * @author <a href="http://github.com/kuujo">Jordan Halterman</a>
  */
 public class NettyTcpProtocolClient implements ProtocolClient {
+  protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
   private final String host;
   private final int port;
-  private final NettyTcpProtocol protocol;
   private final EventLoopGroup group;
   private final Bootstrap bootstrap;
   private Channel channel;
-  private ChannelHandlerContext context;
-  private final Map<Object, CompletableFuture<ByteBuffer>> responseFutures = new HashMap<>(1000);
-  private long requestId;
+  private final Map<Object, CompletableFuture<ByteBuffer>> responseFutures = new ConcurrentHashMap<>(1000);
+  private final AtomicLong requestId = new AtomicLong(0);
 
   private final Supplier<ChannelInboundHandlerAdapter> channelHandlerSupplier = () -> new SimpleChannelInboundHandler<byte[]>() {
-    @Override
-    public void channelActive(ChannelHandlerContext context) {
-      NettyTcpProtocolClient.this.context = context;
-    }
 
     @Override
     protected void channelRead0(ChannelHandlerContext context, byte[] message) throws Exception {
@@ -67,6 +65,8 @@ public class NettyTcpProtocolClient implements ProtocolClient {
       CompletableFuture<ByteBuffer> responseFuture = responseFutures.remove(responseId);
       if (responseFuture != null) {
         responseFuture.complete(buffer.slice());
+      } else {
+        LOGGER.warn("No completable future for response Id {}", responseId);
       }
     }
   };
@@ -75,7 +75,6 @@ public class NettyTcpProtocolClient implements ProtocolClient {
   public NettyTcpProtocolClient(String host, int port, NettyTcpProtocol protocol) {
     this.host = host;
     this.port = port;
-    this.protocol = protocol;
 
     final SslContext sslContext;
     if (protocol.isSsl()) {
@@ -129,14 +128,14 @@ public class NettyTcpProtocolClient implements ProtocolClient {
   public CompletableFuture<ByteBuffer> write(ByteBuffer request) {
     final CompletableFuture<ByteBuffer> future = new CompletableFuture<>();
     if (channel != null) {
-      long requestId = ++this.requestId;
+      long requestId = this.requestId.incrementAndGet();
       ByteBuffer buffer = ByteBuffer.allocate(request.limit() + 8);
       buffer.putLong(requestId);
       buffer.put(request);
+      responseFutures.put(requestId, future);
       channel.writeAndFlush(buffer.array()).addListener((channelFuture) -> {
-        if (channelFuture.isSuccess()) {
-          responseFutures.put(requestId, future);
-        } else {
+        if (!channelFuture.isSuccess()) {
+          responseFutures.remove(requestId);
           future.completeExceptionally(new ProtocolException(channelFuture.cause()));
         }
       });
